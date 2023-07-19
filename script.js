f07099080e93a9b3e39417f5a75ac30c48f92ca7
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-19 20:39:57 +0530

    all done

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ const startQuiz = () => {
 }
 
 const showQuestion = () => {
+    resetState()
     let currentQuestion = questions[currentQuestionIndex]
     let questionNo = currentQuestionIndex + 1
     questionElement.innerHTML = questionNo + '. ' + currentQuestion.question
@@ -61,7 +62,59 @@ const showQuestion = () => {
         button.innerHTML = answer.text
         button.classList.add('btn')
         answerButton.appendChild(button)
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
     })
 }
 
-showQuestion()
+const selectAnswer = (e) => {
+    const selectedBtn = e.target
+    const isCorrect = selectedBtn.dataset.correct === 'true'
+    if (isCorrect) {
+        selectedBtn.classList.add('correct')
+        score++;
+    } else {
+        selectedBtn.classList.add('incorrect')
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if (button.dataset.correct === 'true') {
+            button.classList.add('correct')
+        }
+        button.disabled = true;
+    })
+    nextButton.style.display = 'block'
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}`
+    nextButton.innerHTML = 'Play Again'
+    nextButton.style.display = 'block'
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore()
+    }
+}
+
+nextButton.addEventListener('click', () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton()
+    } else {
+        startQuiz()
+    }
+})
+
+function resetState() {
+    nextButton.style.display = 'none'
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild)
+    }
+}
+startQuiz()
